de9932bd98685acbe008b2ddc31feb77dff97a20
Minh Trang <minhtrang.15809262@gmail.com>
2022-08-19 00:03:01 +0700

    Init automation framework

---
+++ testcases/com/nopcommerce/admin/User01_register_login.java
@@ -0,0 +1,25 @@
+package com.nopcommerce.admin;
+
+import org.testng.annotations.Test;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.AfterClass;
+
+public class User01_register_login {
+	
+  @BeforeClass
+  public void beforeClass() {
+  }	
+  
+  @Test
+  public void TC_01() {
+  }
+  @Test
+  public void TC_02() {
+  }
+
+
+  @AfterClass
+  public void afterClass() {
+  }
+
+}
