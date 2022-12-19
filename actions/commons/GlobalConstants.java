2efce9ab08c07f7a5c4daff71b424b2649fc2bd7
Minh Trang <minhtrang.15809262@gmail.com>
2022-12-19 15:24:12 +0700

    Update GlobalConstant

--- actions/commons/GlobalConstants.java
+++ actions/commons/GlobalConstants.java
@@ -14,7 +14,7 @@ public class GlobalConstants {
 	public static final String ADMIN_PAGE_URL = "https://admin-demo.nopcommerce.com/";
 	public static final String PROJECT_PATH = System.getProperty("user.dir"); 
 	public static final String UPLOAD_FILE_FOLDER = PROJECT_PATH + File.separator + "uploadFiles"; 
-	public static final String DOWNLOAD_FILE_FOLDER = PROJECT_PATH + File.separator + "downloadFiles"; 
+	public static final String DOWNLOAD_FILE_FOLDER = PROJECT_PATH + File.separator + "downloadFiles n"; 
 	
 	
 }
